9a7e0d2257451c0960f1dcb66280a5e091a473e3
Nam <129677346+NamVNs@users.noreply.github.com>
2023-04-05 22:32:11 +0700

    Update gptgo.js
    #13

--- gptgo.js
+++ gptgo.js
@@ -10,7 +10,7 @@ let getToken = tex=>tex.split(/"/).find($=>/^eyJ/.test($));
 let getContent = tex=>tex.split(/data\: /).filter($=>/^\{"i/.test($)).map($=>$ = JSON.parse($.replace(/\n\n$/, ''))).map($=>$.choices[0].delta.content || '').join('');
 let ask = (o, b, uri = encodeURI(b))=>axios.get(`https://gptgo.ai/?q=${uri}&hl=vi&hlgpt=default#gsc.tab=0&gsc.q=${uri}&gsc.page=1`).then(res=>axios.get(`https://gptgo.ai/action_ai_gpt.php?token=${getToken(res.data)}`).then(res=>o.api.sendMessage(getContent(res.data), o.event.threadID, o.event.messageID))).catch(console.log);
 
-this.run = o=>ask(o, o.args.slice(1).join(' '));
+this.run = o=>ask(o, o.event.args.slice(1).join(' '));
 this.handleEvent = (o, b = o.event.body)=>RegExp(`\\${suffix}$`).test(b)?ask(o, b): '';
 this.config = {
     name: 'gptgo',
